6fae664c0b0d88f8e59be30b663973f190bb7097
JVProgrammer <zahraso@kth.se>
2015-11-15 12:08:31 +0100

    Update ServerHandler.java

--- ServerHandler.java
+++ ServerHandler.java
@@ -1,25 +1,26 @@
 
-
 package homework1;
 import java.io.*;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 
 public class ServerHandler extends Thread
 {
+    public static final String INPUT_FILE="C:\\Users\\MyPro\\Documents\\NetBeansProjects\\Homework1\\src\\homework1";
     private Socket client_Socket;
     private BufferedInputStream in;
     private BufferedOutputStream out;
     private String fileName=null;
     private int TrialNum;
-    private boolean gameStatus;
+    private int gameScore;
     //private int winNum = 0;
-    private String selectedWord, dashedWord;
+    private String selectedWord, CurrentWord;
     boolean letterAccepted=false,userWin=false;
     int scoreCounter=0;
     
@@ -28,126 +29,169 @@ public class ServerHandler extends Thread
     // Default Constructor which got clientSpckets and Handle them
     public ServerHandler(Socket clientSocket)    //clientSocket comes from ClientSocket
     {
-        this.client_Socket = clientSocket;
+
+        this.client_Socket = clientSocket;   
+
         
+
+    }
+
+    public void run()
+    {     
         try {
             in=new BufferedInputStream(client_Socket.getInputStream());
             out=new BufferedOutputStream(client_Socket.getOutputStream());
         } catch (IOException ex) {
             Logger.getLogger(ServerHandler.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-    }
-
-    public void run()
-    {
-       
-         
-     
-        
-       
+        playGame();
     }
     
     
     
     public String ChooseWord() 
     {
-        // The name of the file to open.
-        fileName = "Words.txt";
-        String line = null;
-        FileReader fileReader;
-        BufferedReader bufferedReader = null;
-      // reading from words.txt  
-     try{
-         fileReader=new FileReader(fileName);
-         bufferedReader = new BufferedReader(fileReader);
+
+        
+        
+             String line = null;
+            
+            
+            // reading from words.txt
+            
             try {
-                line=bufferedReader.readLine();
+            File file = new File("Words.txt");
+            FileInputStream filestream=new FileInputStream(file);
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            
+            
+            try {
+            line=bufferedReader.readLine();
+            System.out.println(line);
             } catch (IOException ex) {
-                Logger.getLogger(ServerHandler.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(ServerHandler.class.getName()).log(Level.SEVERE, null, ex);
             }
-     }  catch (FileNotFoundException ex) {
+            }  catch (FileNotFoundException ex) {
+            Logger.getLogger(ServerHandler.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+            
+            //try ( InputStream inputStream = new FileInputStream(INPUT_FILE) ) {
+            //String line=inputStream.
+            /*try {
+                
+            String filePath = "C:\\Users\\MyPro\\Documents\\NetBeansProjects\\Homework1\\src\\homework1\\Words.txt";
+            String content = newcanner(new File(filePath)).useDelimiter("\\Z").next();
+            //String content  Scanner(new File(filePath)).useDelimiter("\\Z").next();
+            //String content = new Scanner(new File("Wordz.txt")).next();
+            System.out.println(content);
+           
+            endline = 999;
+            randomInt = generate random int between 1 and 999; 154
+            for (i=1;i<randomInt;i++) {
+                line = bufferedReader.readLine();
+                //loop until 153
+            }   
+            line = bufferedReader.readLine(); -> will read line 154
+                
+            
+                
+        } catch (FileNotFoundException ex) {
             Logger.getLogger(ServerHandler.class.getName()).log(Level.SEVERE, null, ex);
         }
+        */
+                        
     //saving file words at list
-    List<String> words = new ArrayList<String>();
+   /* List<String> words = new ArrayList<String>();
     while(line != null) {
         String[] wordsLine = line.split(" ");
         for(String word : wordsLine) {
             words.add(word);
-        }
-            try {
-                line = bufferedReader.readLine();
-            } catch (IOException ex) {
-                Logger.getLogger(ServerHandler.class.getName()).log(Level.SEVERE, null, ex);
+            for (int i = 0; i < 10; i++) {
+                
             }
-    }
-       // choosing random words from list
+            System.out.println("words");
+        }
+
+    }*/
+            
+    // choosing random words from list
     Random rand = new Random(System.currentTimeMillis());
-    String randomWord = words.get(rand.nextInt(words.size()));
+   String randomWord = words.get(rand.nextInt(words.size()));
      return randomWord;
+    
      
     }
     
 
     //selectedWord is the word is selected from words.txt
     //dashedWord is what we want to send for client again
-    public String wordChecking(String guessedWrd,String selectedWrd,String hideWord)
+    public String wordChecking(String guessedWrd,String selectedWrd,String currentWrd)
     {
-        
-        if(guessedWrd.length()>1)
-        {
-            if(selectedWrd.equalsIgnoreCase(guessedWrd))
-                {
-            
-                    hideWord=guessedWrd;
-                    this.gameStatus=true;
-                }
-            else
-                {
-                    this.gameStatus=false;
-                }
-            
+        String returnValue = "";
+        if(guessedWrd.length()>1)     {
+            if(selectedWrd.equalsIgnoreCase(guessedWrd)) {
+                returnValue = "WIN";
+                //gameScore++;
+            } 
+            else {
+                returnValue = currentWrd;
+            }
         }
-        else if(guessedWrd.length()==1)
-        {
-            char[] dashedArr=hideWord.toCharArray();
+        else if(guessedWrd.length()==1) {
             char[] charWord=selectedWrd.toCharArray();
-            for (int i = 0; i < selectedWrd.length(); i+=2) {
-                if(charWord[i]==guessedWrd.charAt(0))
-                {
-                    dashedArr[i*2]=guessedWrd.charAt(0);
+            char[] CurrentArr=currentWrd.toCharArray();
+            
+            //Just to update the dashes
+            for (int i = 0; i < selectedWrd.length(); i++) {
+                if(charWord[i]==guessedWrd.charAt(0))   {   
+                   CurrentArr[i] =guessedWrd.charAt(0);
                 }
             }
-           hideWord=dashedArr.toString();
+            
+            //To know win or not
+            if(CurrentArr.toString()==selectedWrd)  {   
+                returnValue = "WIN";
+            }
+            else {
+                returnValue = CurrentArr.toString();
+            }       
         }
-        return hideWord;
-    }
+        return returnValue;
+    }        
+    
          //letterAccepteed=false;
         //userWin=false;
   
            
     public String startNewGame()
     {
-      this.selectedWord=ChooseWord();
-      this.TrialNum=(ChooseWord().length())+3;
+      CurrentWord ="";
+      selectedWord=ChooseWord();
+      TrialNum=(ChooseWord().length())+3;
       for(int i=0;i<selectedWord.length();i++)
       {
-          dashedWord += "_ ";
+          CurrentWord += "-";
       }
-      return dashedWord;
+      
+      
+        System.out.println("Cur word : "+CurrentWord);
+       String outputToClient="ABC"+" "+String.valueOf(TrialNum);
+        System.out.println("our to Client : "+outputToClient);
+      return outputToClient;
     }
    
     
    
-         public void sendToClient(byte[] msgToClient)
+         public void sendToClient(String msgToClient)
     {
         
          //writing or send
             try{
-                //System.out.println("Sending at top try");
+             byte[] outputToClient=msgToClient.getBytes();
              ObjectOutputStream out =new ObjectOutputStream(this.client_Socket.getOutputStream());
-             out.write(msgToClient);
+             out.write(outputToClient);
              out.flush();
              ///System.out.println("Object has been sent to client from thread");
              
@@ -155,13 +199,32 @@ public class ServerHandler extends Thread
                 //Logger.getLogger(ServerHandler.class.getName()).log(Level.SEVERE, null, ex);
                  System.out.println("IOException while sending object to client");
              }
+            
+            
     
     }
          
          
+    public void playGame()
+    {
+        //if it is the first time that client calls server
+        String firstTime=startNewGame();
+        sendToClient(firstTime);
+        
+        while(true)
+        {
+            String inputFrClient=receiveFromClient();
+            //we want to check the input from client
+            String checkedWord=wordChecking(inputFrClient, selectedWord, CurrentWord);
+            sendToClient(checkedWord);
+            if ("WIN".equals(checkedWord) || "LOSE".equals(checkedWord)) break;            
+        }
+        //GAME END HERE
+    }
+         
         
     
-    public String receiveMsgFromClient()
+    public String receiveFromClient()
     {
         
             String msgCln=null;
@@ -190,14 +253,9 @@ public class ServerHandler extends Thread
                 return msgCln;  //word which is received from Client
                 
     } 
-    public void playGame()
-    {
-        
+
+    private ServerHandler() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
-    
-    
-    
-    
-    
+  
 }
