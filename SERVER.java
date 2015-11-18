743cc68137306bc231127435a5535a3556390a85
JVProgrammer <zahraso@kth.se>
2015-11-18 15:06:11 +0100

    Update SERVER.java

--- SERVER.java
+++ SERVER.java
@@ -1,35 +1,42 @@
 
 package homework1;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.*;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Server
 {
     private ServerSocket serverSocket=null;
-    private static final int port=1234;
+    private static final int port=4444;
     boolean listening = true;
 
     public static void main(String[] args) throws IOException
-    {
-        
+    {   
        new Server();
-
     }
     
     public Server()
     {
-        
+       /* try {
+            //String content = new Scanner(new File("Words.txt")).useDelimiter("\\Z").next();
+            String content = new Scanner(new File("Words.txt")).next();
+            System.out.println(content);
+        }
+        catch (FileNotFoundException ex) {
+            Logger.getLogger(ServerHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }*/
         
        connection_crt();
+       Listener_crt();
     }
     
-   public void connection_crt(){
-       
-       
+    public void connection_crt(){   
         try {
             serverSocket = new ServerSocket(port);
         } catch (IOException ex) {
@@ -41,9 +48,11 @@ public class Server
    public void Listener_crt(){
        try {
        while(listening)
+           
        {
            
                Socket clientSocket=serverSocket.accept();
+               System.out.println("Client connected");
                new Thread(new ServerHandler(clientSocket)).start();
                
              
